6b3ee6695dcf6e85e67214120f69e49eef150c65
Kyle <notxap55@yahoo.com>
2022-12-09 15:39:08 -0800

    starting basic board functionality

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 'use strict';
 
+//Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
+
+// Modules
 const gameBoard = (() => {
-  const board = [
-    null, null, null,
-    null, null, null,
-    null, null, null
-  ]
+  const board = ['X', 'O', '', '', '', '', '', '', ''];
 
   return { board };
 })();
 
+const displayController = (() => {
+  const spaces = document.querySelectorAll('.space');
+
+  for (let i = 0; i < spaces.length; i++) {
+    spaces[i].textContent = gameBoard.board[i]
+  }
+  return { spaces }
+})();
+
+// gameBoard.board[0] = 'x'
+
+// Factories
+const player = (name, marker) => {
+  return { name, marker }
+}
+
+const player1 = player('player1', 'X');
