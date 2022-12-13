1654059cd5f87acff626a9fb71c2d01107e8a2f3
Kyle <notxap55@yahoo.com>
2022-12-13 14:17:36 -0800

    basic functionality added

--- script.js
+++ script.js
@@ -2,27 +2,60 @@
 
 //Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
 
-// Modules
+
 const gameBoard = (() => {
-  const board = ['X', 'O', '', '', '', '', '', '', ''];
+  const board = ['', '', '', '', '', '', '', '', ''];
 
   return { board };
 })();
 
+
+
 const displayController = (() => {
   const spaces = document.querySelectorAll('.space');
-
-  for (let i = 0; i < spaces.length; i++) {
-    spaces[i].textContent = gameBoard.board[i]
+  const displayMarkers = () => {
+    for (let i = 0; i < spaces.length; i++) {
+      spaces[i].textContent = gameBoard.board[i]
+    }
   }
-  return { spaces }
+  return { spaces, displayMarkers }
 })();
 
-// gameBoard.board[0] = 'x'
 
-// Factories
+
 const player = (name, marker) => {
   return { name, marker }
 }
 
 const player1 = player('player1', 'X');
+const player2 = player('player2', 'O');
+
+const game = (() => {
+  // const checkWinStatus = () => {
+  //   if (gameBoard.board[0] === && gameBoard.board[1]])
+  // }
+
+  let player = player1;
+
+  displayController.spaces.forEach(element => {
+    element.addEventListener('click', (e) => {
+      // add player marker to gameBoard array based on element's index
+      let index = Array.from(displayController.spaces).indexOf(e.target);
+
+      if (gameBoard.board[index] === '') {
+        gameBoard.board[index] = player.marker;
+        player === player1 ? player = player2 : player = player1; // change players
+        displayController.displayMarkers() // load markers
+      } else return
+    })
+  });
+  return { player }
+})()
+
+
+
+// gameBoard.board[5] = 'x';
+// displayController.displayMarkers();
+// gameBoard.board[6] = 'o';
+// displayController.displayMarkers();
+// console.log(player1.marker);
