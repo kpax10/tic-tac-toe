d856b2bbe94cd2316874faa9671600269a8398f0
Kyle <notxap55@yahoo.com>
2022-12-13 15:59:03 -0800

    working on winning conditions

--- script.js
+++ script.js
@@ -31,9 +31,22 @@ const player1 = player('player1', 'X');
 const player2 = player('player2', 'O');
 
 const game = (() => {
-  // const checkWinStatus = () => {
-  //   if (gameBoard.board[0] === && gameBoard.board[1]])
-  // }
+  const checkWinStatus = () => {
+    //loop over board array, if win conditions exist, announce winner
+    var winConditions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]
+    ]
+    // if (arr[0] === player.marker)
+    // NOT WORKING
+    for (let i = 0; i < winConditions.length - 1; i++) {
+      for (let j = 0; j < winConditions[i].length; j++) {
+        if (gameBoard.board[i] === 'X') console.log('win');
+        // if (arr[j] === 'X') {
+        //   console.log("winner");
+        // } else return
+      }
+    }
+  }
 
   let player = player1;
 
@@ -45,7 +58,8 @@ const game = (() => {
       if (gameBoard.board[index] === '') {
         gameBoard.board[index] = player.marker;
         player === player1 ? player = player2 : player = player1; // change players
-        displayController.displayMarkers() // load markers
+        displayController.displayMarkers();
+        checkWinStatus(); // load markers
       } else return
     })
   });
